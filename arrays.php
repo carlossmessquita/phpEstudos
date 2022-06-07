0d754dd4a0a35cd3d479177446ce1f253daf0065
carlossmsqt <carlossmessquita@gmail.com>
2022-06-07 15:48:49 -0300

    Dia 02 - Estudos PHP

---
+++ arrays.php
@@ -0,0 +1,52 @@
+<?php
+
+    // Arrays no PHP:
+    $arrayA = array(10, 20, 30);
+    $arrayB = [40, 50, 60];
+
+    //echo $arrayA[0].PHP_EOL;
+    //echo $arrayB[0].PHP_EOL;
+
+    for($i = 0; $i < count($arrayA); $i++) {
+        echo $arrayA[$i].PHP_EOL;
+    }
+
+    for($i = 0; $i < count($arrayB); $i++) {
+        echo $arrayB[$i].PHP_EOL;
+    }
+
+    // Array Associativo -> Similar ao Dicionário Python:
+    $arrAssociativo1 = [
+        'nome' => 'Carlos',
+        'idade' => 22
+    ];
+    $arrAssociativo2 = [
+        'nome' => 'Vitor',
+        'idade' => 22
+    ];
+    $arrAssociativo3 = [
+        'nome' => 'Silva',
+        'idade' => 22
+    ];
+
+    echo $arrAssociativo1['nome'].PHP_EOL;
+    echo $arrAssociativo2['idade'].PHP_EOL;
+
+    // Array de Arrays:
+    $matriz = [$arrAssociativo1, $arrAssociativo2, $arrAssociativo3];
+
+    echo $matriz[0]['nome'].PHP_EOL;
+    echo $matriz[0]['idade'].PHP_EOL;
+
+    // Foreach
+    foreach($matriz as $mat) {
+        echo $mat['nome'].PHP_EOL;
+    }
+
+    // Add. itens ao array:
+    $arrayA[] = 0;
+    $arrayB[] = 0;
+
+    foreach($arrayA as $arr) {
+        echo $arr.PHP_EOL;
+    }
