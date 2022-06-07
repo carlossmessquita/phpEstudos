569a7408ac8b3ce1398eb31a3af9d9d9f7a142ec
carlossmsqt <carlossmessquita@gmail.com>
2022-06-07 18:23:54 -0300

    Pequenas alterações

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php
 
-    echo "Hello, World\n";
+echo "Hello, World\n";
 
-    class Main{
-        public function __construct()
-        {
-            echo "Hello, ";
-        }
-        /**
-         * Doc function
-         *
-         * @param string $name
-         * @return void
-         */
-        public function teste(string $name): void
-        {
-            echo $name;
-        }
+class Main{
+    public function __construct()
+    {
+        echo "Hello, ";
     }
-    
-    (new Main())->teste("Carlos");
+    /**
+     * Doc function
+     *
+     * @param string $name
+     * @return void
+     */
+    public function teste(string $name): void
+    {
+        echo $name;
+    }
+}
+
+(new Main())->teste("Carlos");
 
